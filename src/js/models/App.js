6986fb0ed3a0f921eb0f470b2a4ad408b2d9f3ad
Les Orchard <me@lmorchard.com>
2015-07-28 17:41:38 -0400

    First stab at making workout progress persistent. Probably over-engineered.

--- src/js/models/App.js
+++ src/js/models/App.js
@@ -1,6 +1,11 @@
+var Promise = require('es6-promise').Promise;
 var State = require('ampersand-state');
 var Collection = require('ampersand-collection');
 var WorkoutCollection = require('./WorkoutCollection');
+var throttle = require('lodash.throttle');
+
+var SAVE_THROTTLE_DELAY = 1000;
+var LS_KEY_PREFIX = 'c25k-workout-';
 
 module.exports = State.extend({
 
@@ -10,9 +15,71 @@ module.exports = State.extend({
   },
 
   initialize: function () {
-    var DATA = require('../../data/c25k.json');
-    this.title = DATA.title;
-    this.workouts = new WorkoutCollection(DATA.workouts);
+
+    this.workouts = new WorkoutCollection();
+
+    this.loadWorkoutDefinitions().then((data) => {
+
+      this.title = data.title;
+      this.workouts.set(data.workouts);
+
+      return this.loadWorkoutsProgress();
+
+    }).then((progressData) => {
+
+      // If we have any progress data, update the workouts.
+      if (progressData.length) {
+        progressData.forEach((item) => {
+          if (!item.elapsed) { return; }
+          var workout = this.workouts.get(item.id);
+          if (!workout) { return; }
+          workout.elapsed = item.elapsed;
+        });
+      }
+
+      // Set up a listener to periodically save progress as it changes
+      this.listenTo(this.workouts, 'change', throttle((ev) => {
+        this.saveWorkoutsProgress().then(() => {
+          console.log('Saved workout progress');
+        });
+      }, SAVE_THROTTLE_DELAY));
+
+    }).catch((err) => {
+      // TODO: Will need an actual error handler.
+      console.error(err);
+    });
+
+  },
+
+  loadWorkoutDefinitions: function () {
+    // TODO: use fetch(), and localize?
+    return new Promise((resolve, reject) => {
+      return resolve(require('../../data/c25k.json'));
+    });
+  },
+
+  loadWorkoutsProgress: function () {
+    // TODO: Use a pluggable server?
+    return Promise.all(this.workouts.map((workout) => {
+      return new Promise((resolve, reject) => {
+        var dataTxt = window.localStorage.getItem(LS_KEY_PREFIX + workout.id);
+        resolve(dataTxt ? JSON.parse(dataTxt) : {});
+      });
+    }));
+  },
+
+  saveWorkoutsProgress: function () {
+    // TODO: Use a pluggable server?
+    return Promise.all(this.workouts.map((workout) => {
+      return new Promise((resolve, reject) => {
+        var data = {
+          id: workout.id,
+          elapsed: workout.elapsed
+        };
+        window.localStorage.setItem(LS_KEY_PREFIX + workout.id, JSON.stringify(data));
+        resolve(true);
+      });
+    }));
   }
 
 });
